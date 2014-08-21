0ba351d4aff14054a6c237d304a7262d7a7a05c3
t-doi <doi@neptune.kanazawa-it.ac.jp>
2014-08-21 13:54:09 +0900

    Win8.1 64bit 対応

--- MinGWコンパイル環境セットアップ.bat
+++ MinGWコンパイル環境セットアップ.bat
@@ -1,8 +1,10 @@
 @echo OFF
 title MinGW-gcc
 echo MinGW-gccのディレクトリにパスを通します 
-path C:\Program Files\CodeBlocks\MinGW\bin;%PATH%
+rem path C:\Program Files\CodeBlocks\MinGW\bin;%PATH%
+path C:\Program Files (x86)\CodeBlocks\MinGW\bin;%PATH%
 rem path
+set LANG=ja_JP.UTF-8
 echo ------- gcc check ------------
 gcc -v
 rem pause
